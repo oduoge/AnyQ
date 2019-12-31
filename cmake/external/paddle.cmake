0513e0e148482736c7a192cedaa06129fc3512ae
oduoge <oduoge@126.com>
2019-12-31 14:58:19 +0800

    淇敼paddle渚濊禆

--- cmake/external/paddle.cmake
+++ cmake/external/paddle.cmake
@@ -7,7 +7,7 @@ ExternalProject_Add(
     extern_paddle
     ${EXTERNAL_PROJECT_LOG_ARGS}
     GIT_REPOSITORY       "https://github.com/PaddlePaddle/Paddle.git"
-    GIT_TAG              "v/0.14.0"
+    GIT_TAG              "v0.14.0"
     PREFIX               ${PADDLE_SOURCES_DIR}
     CONFIGURE_COMMAND    mkdir -p ${PADDLE_INSTALL_DIR} && cd ${PADDLE_INSTALL_DIR} && ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${PADDLE_INSTALL_DIR} 
                          -DCMAKE_BUILD_TYPE=Release -DWITH_PYTHON=OFF -DWITH_MKL=ON -DWITH_MKLDNN=OFF -DWITH_GPU=OFF -DWITH_FLUID_ONLY=ON <SOURCE_DIR>
